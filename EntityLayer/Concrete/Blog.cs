5854310b49c39af22b87475a2a43a4ce5a59fa18
MrYunusEmre <yeertunc63@gmail.com>
2021-11-09 15:24:07 +0300

    initial commit

---
+++ EntityLayer/Concrete/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Blog
+    {
+        public int BlogID { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogThumbnailImage { get; set; }
+        public int BlogImage { get; set; }
+        public string MyProperty { get; set; }
+        public DateTime BlogCreateDate { get; set; }
+        public bool BlogStatus { get; set; }
+    }
+}
