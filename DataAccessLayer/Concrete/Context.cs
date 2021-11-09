5af140819ae475c665abaf716d95d4b380c95772
MrYunusEmre <yeertunc63@gmail.com>
2021-11-09 16:13:06 +0300

    layers are added on each other by using dependencies part. Database string in Context must be updated as local database name.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //add here your local database server name
+            optionsBuilder.UseSqlServer("..;database=CoreBlogDb;integrated security = true");
+
+        }
+        public DbSet<About> Abouts { get; set; }
+
+    }
+}
