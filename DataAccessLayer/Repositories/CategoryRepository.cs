e610275f052a900bafea2f92960be1154198f39a
MrYunusEmre <yeertunc63@gmail.com>
2022-02-20 22:09:11 +0300

    Repositories are created

---
+++ DataAccessLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context context = new Context();
+        public void AddCategory(Category category)
+        {
+            context.Add(category);
+            context.SaveChanges(true);
+        }
+
+        public Category getById(int id)
+        {
+            return context.Categories.Find(id);
+        }   
+
+        public List<Category> ListAllCategory()
+        {
+            return context.Categories.ToList();
+        }
+
+        public void RemoveCategory(Category category)
+        {
+            context.Remove(category);   
+            context.SaveChanges(true);
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            context.Update(category);
+        }
+    }
+}
