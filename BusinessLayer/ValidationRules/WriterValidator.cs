7127fe81e6016dc268a75874279a0a8c6301c3e1
MrYunusEmre <yeertunc63@gmail.com>
2022-03-07 18:34:57 +0300

    Fluent validation is updated

--- BusinessLayer/ValidationRules/WriterValidator.cs
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BusinessLayer.ValidationRules
@@ -12,13 +13,45 @@ namespace BusinessLayer.ValidationRules
     {
         public WriterValidator()
         {
-            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar Adı Soyadı Kısmı Boş Geçilemez");
-            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail Adresi Boş Geçilemez.");
-            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre Boş Geçilemez.");
-            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter veri girişi yapınız.");
-            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Lütfen en fazla 50 karakter veri girişi yapınız.");
+
+
+            RuleFor(x => x.WriterName).NameSurname();
+            RuleFor(x => x.WriterMail).Mail();
+            RuleFor(x => x.WriterPassword).Password();
             RuleFor(x => x.WriterCity).NotEmpty().WithMessage("Şehir boş geçilemez.");
 
+
+        }
+    }
+    public static class RuleBuilderExtensions
+    {
+        public static IRuleBuilder<T, string> Password<T>(this IRuleBuilder<T, string> ruleBuilder, int minimumLength = 6,int maximumLength = 50)
+        {
+            var options = ruleBuilder
+                .NotEmpty().WithMessage("Şifre boş geçilemez.")
+                .MinimumLength(minimumLength).WithMessage($"Şifre en az {minimumLength} karakter uzunluğunda olmalıdır")
+                .Matches("[A-Z]").WithMessage("Şifre en az 1 adet büyük harf içermelidir.")
+                .Matches("[a-z]").WithMessage("Şifre en az 1 adet küçük harf içermelidir.")
+                .Matches("[0-9]").WithMessage("Şifre en az 1 adet rakam içermelidir");
+                //.Matches("[^a-zA-Z0-9]").WithMessage("");
+            return options;
+        }
+
+        public static IRuleBuilder<T, string> NameSurname<T>(this IRuleBuilder<T, string> ruleBuilder, int minimumLength = 2, int maximumLength = 50)
+        {
+            var options = ruleBuilder
+                .NotEmpty().WithMessage("Yazar adı soyadı kısmı boş geçilemez.")
+                .MinimumLength(minimumLength).WithMessage($"Yazar adı soyadı en az {minimumLength} karakter uzunluğunda olmalıdır.")
+                .MaximumLength(maximumLength).WithMessage($"Yazar adı soyadı en fazla {maximumLength} karakter uzunluğunda olmalıdır.");
+            //.Matches("[^a-zA-Z0-9]").WithMessage("");
+            return options;
+        }
+
+        public static IRuleBuilder<T, string> Mail<T>(this IRuleBuilder<T, string> ruleBuilder, int minimumLength = 2)
+        {
+            var options = ruleBuilder
+                .NotEmpty().WithMessage("Yazar mail kısmı boş geçilemez.");
+            return options;
         }
     }
 }
